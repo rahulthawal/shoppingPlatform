1db86e353fd61cb1c2b2ac1feecb7dcee2c40bd4
rahulthawal <iamrahulthawal@gmail.com>
2019-08-24 10:11:30 +0530

    Shopping Cart details added.

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../services/shopping-cart/shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent {
-  constructor() {}
+export class ShoppingCartComponent implements OnInit {
+  cart$;
+
+  constructor(private cartService: ShoppingCartService) {}
+  async ngOnInit() {
+    this.cart$ = await this.cartService.getCart();
+  }
 }
